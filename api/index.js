dc9bd9728d3ee1c49bf37500ac2e11a694bd2c58
Dddiko07 <diko.23285@mhs.unesa.ac.id>
2026-02-13 17:52:43 +0700

    fix vercel serverless export

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
-// api/index.js
 require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
 
 const { connectDB } = require("../src/config/db");
-
 const authRoutes = require("../src/routes/authRoutes");
 const resiRoutes = require("../src/routes/resiRoutes");
 
@@ -14,16 +12,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// connect db
+// connect database
 connectDB();
 
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/resi", resiRoutes);
 
-// test
 app.get("/", (req, res) => {
-  res.send("Return Ukhti Khadijah API running (Vercel)");
+  res.send("Return Ukhti Khadijah API running");
 });
 
-module.exports = app;
+module.exports = app; // INI PENTING
